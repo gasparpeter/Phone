4c3b045cac34de012ba410313523ead0c425bd22
gasparpeter <petihappygaspar@gmail.com>
2019-09-19 15:37:34 +0300

    - passcode input

---
+++ script.js
@@ -0,0 +1,7 @@
+let phoneContainer = document.getElementById('phone-container');
+let screenContainer = document.getElementById('screen-container');
+let pinContainer = document.getElementById('pin');
+let utasitas = document.getElementById('utasitas');
+let h1 = document.getElementById('inputka')
+let homeBtn = document.getElementById('home-btn');
+
