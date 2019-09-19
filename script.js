d5f71d5c52562c12bbf2fbe69368c85e3cf39785
gasparpeter <petihappygaspar@gmail.com>
2019-09-19 16:35:53 +0300

    - done btn

--- script.js
+++ script.js
@@ -4,8 +4,18 @@ let pinContainer = document.getElementById('pin');
 let utasitas = document.getElementById('text');
 let input = document.getElementById('inputka');
 let homeBtn = document.getElementById('home-btn');
+let doneBtn = document.getElementById('done');
+
+let passcode = "akph";
 
 homeBtn.addEventListener('click', function () {
    pinContainer.style.display = 'block';
 });
 
+doneBtn.addEventListener('click', function () {
+   if (input.value === passcode) {
+       alert('jo')
+   }else {
+       alert('nem jo')
+   }
+});
