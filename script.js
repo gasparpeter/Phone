827e0fb12a08764913ba58bc21307af17315e4ee
gasparpeter <petihappygaspar@gmail.com>
2019-09-20 11:01:04 +0300

    - top items

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ let doneBtn = document.getElementById('done');
 let homeScreen = document.getElementById("home-screen");
 let powerBtn = document.getElementById('powerBtn');
 
+let telephone = document.getElementById('telephone');
+let safari = document.getElementById('safari');
+let music = document.getElementById('music');
+let messages = document.getElementById('messages');
+
 var audio = new Audio('iPhonelock.mp3');
 
 let passcode = "akph";
@@ -16,10 +21,13 @@ homeBtn.addEventListener('click', function () {
    pinContainer.style.display = 'block';
 });
 
+
+
 doneBtn.addEventListener('click', function () {
    if (input.value === passcode) {
        pinContainer.style.display = 'none';
        homeScreen.style.display = 'block';
+       topItems();
    }else {
        input.style.border = '1px solid red'
    }
@@ -31,3 +39,10 @@ powerBtn.addEventListener('click', function () {
    input.value = null;
    audio.play();
 });
+
+function topItems() {
+    telephone.style.display = 'inline-block';
+    safari.style.display = 'inline-block';
+    music.style.display = 'inline-block';
+    messages.style.display = 'inline-block';
+}
