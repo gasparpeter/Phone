9cb89e4d8571c27a3b6bd2b535859d20b910dac4
gasparpeter <petihappygaspar@gmail.com>
2019-09-30 14:57:46 +0300

    - list added

--- script.js
+++ script.js
@@ -34,6 +34,21 @@ let music = document.getElementById('music');
 let messages = document.getElementById('messages');
 
 
+// SMS
+let list = document.getElementById('list');
+let container = document.getElementById('container');
+
+let name = document.getElementById('input');
+let message = document.getElementById('textarea');
+let sendBtn = document.getElementById('send');
+
+let SMS = [];
+
+let editmode = false;
+let editIndex = null;
+
+
+
 
 
 var audio = new Audio('iPhonelock.mp3');
@@ -52,6 +67,9 @@ homeBtn.addEventListener('click', function () {
 
 
 doneBtn.addEventListener('click', function () {
+
+
+
    if (input.value === passcode) {
        pinContainer.style.display = 'none';
        homeScreen.style.display = 'block';
@@ -137,4 +155,65 @@ messages.addEventListener('click', function () {
     messageScreen.style.display = 'block';
 });
 
+sendBtn.addEventListener('click', function () {
 
+    if ( editmode && editIndex !== null ) {
+
+        list.children[ editIndex ].innerText = name.value;
+
+        SMS[ editIndex ].name = name.value;
+        SMS[ editIndex ].message = message.value;
+
+    }else {
+        if (name.value.length <= 0 || message.value.length <= 0) {
+            alert('ERROR')
+        }else {
+            const names = document.createElement('li');
+            names.innerText = name.value;
+            list.appendChild(names);
+
+
+            SMS.push({
+                "name": name.value,
+                "message": message.value
+            });
+
+            names.addEventListener('click',  function (e) {
+                const index = getChildIndex( e.target );
+
+                const sms = SMS[ index ];
+
+                name.value = sms.name;
+                message.value = sms.message;
+
+                editmode = true;
+                editIndex = index;
+            });
+
+            resetInputs()
+        }
+
+        container.style.display = 'none';
+        list.style.display = 'block';
+    }
+
+
+
+});
+
+var getChildIndex = function(child){
+    var parent = child.parentNode;
+    var children = parent.children;
+    var i = children.length - 1;
+    for (; i >= 0; i--){
+        if (child == children[i]){
+            break;
+        }
+    }
+    return i;
+};
+
+function resetInputs() {
+    name.value = null;
+    message.value = null;
+}
